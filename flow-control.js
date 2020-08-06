4ad67861d351cea7cc8d7b7db77982ac014e1259
Dona F <donafrancisco1991@gmail.com>
2020-08-06 16:58:30 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,1 +1,1 @@
-function basicTeenager(basicTeenager){ if (basicTeenager > 13) return "You are a teenager"}
+function basicTeenager(basicTeenager){ if (basicTeenager > 13 && basicTeenager <19 ) return "You are a teenager"}
